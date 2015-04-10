38a2c4b5db3a73402424af2dacbc33bb5b2ed255
Michael Desa <mjdesa@gmail.com>
2015-04-09 19:32:57 -0700

    Add basic login stuff

--- app.js
+++ app.js
@@ -1,14 +1,53 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var db = require('./models');
+var bodyParser = require('body-parser');
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
 app.get('/', function(req,res){
 	res.render("index", {title: "My title"});
 });
+
+
+app.get('/login', function(req,res){
+	res.render("user/login");
+});
+
+app.get('/signup', function(req,res){
+	res.render("user/signup");
+});
+
+app.get('/profile', function(req,res){
+	res.send("I'm a profile");
+});
+
+app.post('/login', function(req,res){
+	res.send("I'm a login");
+});
+
+// GET /user/:id ---> req.params.id
+// GET /user -------> req.query.id
+// POST /user ------> req.body.id
+
+app.post('/signup', function(req,res){
+	var email = req.body.email;
+	var password = req.body.password;
+	db.User.createSecure(email,password)
+	  .then(function(user){
+	  	res.redirect('/profile');
+	  });
+});
+
+app.delete('/logout', function(req,res){
+	res.send("I'm a delete");
+});
+
 
 app.get('/search',function(req,res){
 	var movieSearch = req.query.q3;
